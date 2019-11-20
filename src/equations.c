d7e4b6659392a72e2307387ce43c19c8d3160c8c
Oscar VERETOUT <overetou@e3r5p3.42.fr>
2019-11-20 19:46:29 +0100

    a bit of messing around

--- src/equations.c
+++ src/equations.c
@@ -2,13 +2,22 @@
 
 BOOL    try_var_as_unknown(t_master *m, char *name)
 {
+    t_content content;
+
     if (m->to_define)
     {
         handle_line_error(m, "Too many unknown variables.");
         return (0);
     }
     m->to_define = name;
-    inject_value(m, (t_content)NULL, UNKNOWN);
+    content.flt = 1;
+    inject_value(m, content, UNKNOWN);
 }
 
-//operation cases with unknowns.
+void    multiply_anytype_by_unknown(t_expr *e1, t_expr *e2)
+{
+    if (e1->info == UNKNOWN)
+        swap_pointer(&e1, &e2);//Now we have the garantee that e2 is an unkown
+    e1->unknown_degree += e2->unknown_degree;
+    e2->unknown_degree = 0;
+}
